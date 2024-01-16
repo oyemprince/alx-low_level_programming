3f8fe11a22d80e443fd801630a891745a6a3d401
root <oyeyemigabriel97@gmail.com>
2024-01-16 04:43:30 -0800

    updating with new tasks

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*print_sign-prints sign of a number
+*@c: character to be verified
+*Return: return 0 or 1
+*/
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return -1;
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return 1;
+	else
+	{
+		_putchar('0')
+		return 0;
+	}
+}
