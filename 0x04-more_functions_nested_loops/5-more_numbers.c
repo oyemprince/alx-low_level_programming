5cda956a431333ef90202599e297f595b4842e00
root <oyeyemigabriel97@gmail.com>
2024-01-20 03:27:54 -0800

    fixing task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,19 @@
 */
 void more_numbers(void)
 {
-	int number = 48;
+int number1, number2;
 
-	for (int i = 1; i <= 10; i++)
-		{
-			for (number = 48; number < 63; number++)
-				{
-					_putchar(number);
-				}
-			_putchar(10);
-		}
+for (number1 = 0; number1 < 10; number1++)
+{
+for (number2 = 0; number2 <= 14; number2++)
 
+{
+if (number2 > 9)
+{
+_putchar((number2 / 10) + '0');
+}
+_putchar((number2 % 10) + '0');
+}
+_putchar(10);
+}
 }
