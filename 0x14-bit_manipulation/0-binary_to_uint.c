4c3798c262a189e30dbc5d50106c185a5e873d04
root <oyeyemigabriel97@gmail.com>
2024-02-16 22:52:01 -0800

    fixing task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,46 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
-
 /**
-  * binary_to_uint - Converts a binary number to an unsigned int
-  * @b: The binary string to converts
-  *
-  * Return: The positive number converted from a binary
-  */
+ * binary_to_uint - converts a binary to an unsigned int
+ * @b: the binary number as a string
+ *
+ * Return: the converted value
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = 0, count = 0, sum = 0;
+unsigned int decimal = 0;
+int str_len = 0, base = 1;
 
-	if (b == NULL)
-		return (0);
+if (!check_valid_string(b))
+return (0);
 
-	len = _strlen(b);
-	while (len--)
-	{
-		if (b[len] != 48 && b[len] != 49)
-			return (0);
+while (b[str_len] != '\0')
+str_len++;
 
-		if (b[len] == 49)
-			sum += 1 << count;
-
-		count++;
-	}
-
-	return (sum);
+while (str_len)
+{
+decimal += ((b[str_len - 1] - '0') * base);
+base *= 2;
+str_len--;
+}
+return (decimal);
 }
 
 /**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
+ * check_valid_string - checks if a string has only 0's and 1's
+ * @b: string to be checked
+ *
+ * Return: 1 if string is valid, 0 otherwise
+ */
+int check_valid_string(const char *b)
 {
-	int c = 0;
+if (b == NULL)
+return (0);
 
-	while (s[c])
-		c++;
-
-	return (c);
+while (*b)
+{
+if (*b != '1' && *b != '0')
+return (0);
+b++;
+}
+return (1);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,39 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "mainn.h"
+#include "main.h"
 
 /**
-  * print_binary - Prints the binary representation of a number
-  * @n: The number to representing in binary
-  *
-  * Return: Nothing
-  */
+*print_binary - prints the binary representation of a number
+*@n: number to be printed
+*
+*Return: void
+*/
+
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+if (n > 1)
+	print_binary(n >> 1);
 
-	_divide(n);
-}
-
-/**
-  * _divide - ...
-  * @n: ...
-  *
-  * Return: ...
-  */
-void _divide(unsigned long int n)
-{
-	if (n < 1)
-		return;
-
-	_divide(n >> 1);
-
-	if (n & 1)
-		_putchar('1');
-	else
-		_putchar('0');
+_putchar((n & 1) + '0');
 }
